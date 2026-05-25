22a3e44a5eae6ad0b78d97e9f4cbc7ae28422cdf
Muhamad Zulhilmi bin Mohamed Yusof <d20231106353@siswa.upsi.edu.my>
2026-05-26 05:02:10 +0800

    Serve compiled Tailwind CSS directly

--- resources/views/mykubur.blade.php
+++ resources/views/mykubur.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>MyKubur - Perkuburan Islam Kampung Rantau Panjang</title>
-    @vite(['resources/css/app.css'])
+    <link rel="stylesheet" href="{{ asset('assets/css/tailwind-compiled.css') }}">
     <!-- Lucide Icons -->
     <script src="https://unpkg.com/lucide@latest"></script>
     <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
