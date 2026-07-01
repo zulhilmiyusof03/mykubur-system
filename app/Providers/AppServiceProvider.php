dd225e1549b9968e57afb3c245ccdab36b656e4f
Muhamad Zulhilmi bin Mohamed Yusof <d20231106353@siswa.upsi.edu.my>
2026-07-02 06:05:50 +0800

    fix https force and render mixed content issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,25 +3,18 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
-        if ($this->app->environment('production')) {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
+        // 🔥 FORCE HTTPS FOR ALL GENERATED URLS
+        URL::forceScheme('https');
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
